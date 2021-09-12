448e10137e060b3854dd055d2345bb21ca077a5e
janos <janos.tran@transwap.com>
2021-09-12 23:23:02 +0700

    Init

---
+++ Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Models
+{
+    /// <summary>
+    /// Custom role id string to guid 
+    /// </summary>
+    public class Role : IdentityRole<Guid>
+    {
+    }
+}
